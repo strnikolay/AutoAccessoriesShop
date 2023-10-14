3e194b979f44319bd9c67830b3b0fb5417799810
Strnikolay <nikolayromanchuk88@gmail.com>
2023-10-14 23:54:48 +0300

    добавление react компонентов

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,12 +1,60 @@
 import "./footer.css"
+import logo from "../../images/white_logo_text.svg"
 
 function MainFooter () {
 
 return(
-   
-  <footer className="footer">
-      <a href="/">&copy; 2023.</a>
+
+  <footer class="footer">
+    <div class="auto-group-eowm-dLj">
+      <img class="logo-NJK" src={logo}/>
+      <p class="pro-auto--7Fu">
+      © 2019 PRO Auto. 
+      <br/>
+      Все права защищены.
+      </p>
+    </div>
+    <div class="auto-group-d6ab-oeX">
+      <div class="item--9M1">КОМПАНИЯ</div>
+      <p class="item--eoZ">О КОМПАНИИ</p>
+      <p class="item--mNP">ЛИЦЕНЗИИ</p>
+      <p class="item--Gpw">ПАРТНЕРЫ</p>
+      <p class="item--bsD">ОТЗЫВЫ КЛИЕНТОВ</p>
+      <p class="item--gtf">РЕКВИЗИТЫ</p>
+      <p class="item--1AF">ВАКАНСИИ</p>
+    </div>
+    <div class="auto-group-p7b5-vo1">
+      <div class="item--TH9">КАТАЛОГ</div>
+      <p class="item--N9D">АККУМУЛЯТОРЫ</p>
+      <p class="item--g9u">АВТОМАСЛА</p>
+      <p class="item--oEX">АКСЕССУАРЫ</p>
+      <p class="item--j8B">АВТОХИМИЯ</p>
+    </div>
+    <div class="auto-group-jmfd-FMR">
+      <div class="item--n6T">УСЛУГИ</div>
+      <p class="item--FVq">ДИАГНОСТИКА АККУМУЛЯТОРА</p>
+      <p class="item--jR1">ПРИЕМ АККУМУЛЯТОРОВ</p>
+    </div>
+    <div class="auto-group-suk9-dWP">
+      <div class="item--9jd">ИНФОРМАЦИЯ</div>
+      <p class="item--4rb">АКЦИИ</p>
+      <p class="item--Pdy">НОВОСТИ</p>
+      <p class="item--WyV">СТАТЬИ</p>
+      <p class="item--FRH">ВОПРОС - ОТВЕТ</p>
+    </div>
+    <div class="auto-group-wwyr-mPd">
+      <img class="vector-Wc7" src="./assets/vector-mVh.png"/>
+      <img class="subtract-4Nj" src="./assets/subtract-q7V.png"/>
+    </div>
+    <div class="auto-group-zscb-znB">
+      <div class="auto-group-jpkd-8dV">
+        <p class="item-7-342-279-36-13-g9D">+7 (342) 279-36-13</p>
+        <p class="proauto59yandexru-PZR">proauto59@yandex.ru</p>
+      </div>
+      <div class="button-irb">ЗАКАЗАТЬ ЗВОНОК</div>
+    </div>
   </footer>
+
 
 )}
 
