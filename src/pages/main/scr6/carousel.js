d4a5caca5fcc63b7243cc14870f948957826f360
Strnikolay <nikolayromanchuk88@gmail.com>
2023-10-30 23:43:17 +0300

    30_10_23

---
+++ src/pages/main/scr6/carousel.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './carousel.css';
+import useTransition from './useTransition';
+
+export default function Carousel({ children, width, unit }) {
+  // here we extracted the carousel functionality into its own hook
+  const {
+    translate,
+    items,
+    setAction,
+  } = useTransition(width, children);
+
+  const handleNext = () => setAction('next');
+  const handlePrev = () => setAction('prev');
+
+  return (
+    <>
+        <div 
+            className="parent" 
+            style={{width: `${width*3}${unit}`}}
+        >
+            <button className="prev" onClick={handlePrev}/>
+
+            <div className="container">
+                <div
+                    className="inner"
+                    style={{
+                        width: `${width * items.length}${unit}`,
+                        transform: `translateX(-${translate}${unit})`,
+                    }}
+                >
+                    {items.map(item => (
+                    <div
+                        className="item"
+                        style={{width: `${width}${unit}`}}
+                    >
+                        {item}
+                    </div>
+                    ))}
+                </div>
+            </div>
+            <button className="next" onClick={handleNext}/>
+        </div>
+    </>  
+)};
+
+Carousel.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired,
+  width: PropTypes.number,
+  unit: PropTypes.string,
+};
+
+Carousel.defaultProps = {
+  width: 300,
+  unit: 'px',
+};
--- src/pages/main/scr6/screen6.jsx
+++ src/pages/main/scr6/screen6.jsx
@@ -6,43 +6,30 @@ import part3 from "./img/part3.png"
 import part4 from "./img/part4.png"
 import part5 from "./img/part5.png"
 import part6 from "./img/part6.png"
+import Carousel from './carousel';
+
 
 function MainSixScreen () {
-  const [Img, setImg] = useState (0) 
-
-
-  //const outImg = document.querySelector('.slider__img')
-
   
-  const images = [
+  /*const images = [
     <img key={part1} scr={part1} />,
     <img key={part2} scr={part2} />,
     <img key={part3} scr={part3} />
-  ]
+  ]*/
+
+  /*const images = [
+    "part1",
+    "part2",
+    "part3",
+    "part4",
+    "part5"
+  ]*/
 
   /*useEffect(() => {
     outImg.innerHTML =`<img src=${images[Img]}>`
   })*/
 
- 
-  
-  const NextHandler = () =>{
-      if(Img < images.length - 1){
-          setImg(Img++)
-      }else{
-          setImg(0)
-      }
-      //outImg.innerHTML =`<img src=${images[Img]}>`
-  }
-  
-  const PrevHandler = () =>{
-      if(Img > 0 ){
-        setImg(Img--)
-      }else{
-          setImg(images.length-1) 
-      }
-      //outImg.innerHTML =`<img src=${images[Img]}>`
-  }
+
 
 
 
@@ -50,25 +37,37 @@ return(
   
   <div className="screen6">
 
-    <div className="wrapper">
+      <Carousel>
+        <img src={part1} />
+        <img src={part2} />
+        <img src={part3} />
+        <img src={part4} />
+        <img src={part5} />
+        <img src={part6} />
+      </Carousel>
+
+    {/*<div className="wrapper">
         <div className="slider">
             <div className="container">
-                <div className="slider__block">
-                    <div className="slider__img">
-                        {images[Img]}
+
+                    <button className="prev" onClick={handlePrev}/>
+                    <div className="slider_img">
+                        {images.map((Img, index)=>
+                             <div key={index} className={Img} />
+                        )}
                     </div>
-                    <div className="slider__btn">
-                        <button className="prev" onClick={PrevHandler}/>
-                        <button className="next" onClick={NextHandler}/>
-                    </div>
-                </div>
+                    <button className="next" onClick={handleNext}/>
+                    
+
 
             </div>
         </div>
-    </div>
+                        </div>*/}
     
   </div>
 
 )}
+
+
 
 export default MainSixScreen;   
