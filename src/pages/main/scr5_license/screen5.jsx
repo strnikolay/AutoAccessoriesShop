133a4e13b05af9fcaf2a802f2c980145222854b4
Strnikolay <nikolayromanchuk88@gmail.com>
2023-11-03 23:51:29 +0300

    03_11_23

---
+++ src/pages/main/scr5_license/screen5.jsx
@@ -0,0 +1,85 @@
+import "./screen5.css"
+
+function MainThirdScreen () {
+
+return(
+  
+  <div className="screen3">
+    
+  <div class="item--bnF">
+    <div class="auto-group-t1vh-XA7">
+      <div class="frame-4-EKR">
+        <div class="group-27-au5">
+          <img class="group-28-wzX" src="./assets/group-28.png"/>
+          <p class="mobil-5L3">
+          Официальный дистрибьютор материалов 
+          <br/>
+          марки Mobil
+          </p>
+        </div>
+      </div>
+      <div class="frame-5-LWs">
+        <div class="group-27-6kw">
+          <img class="group-28-s11" src="./assets/group-28-Bk3.png"/>
+          <p class="liqui-moly-Pzw">
+          Официальный дистрибьютор материалов 
+          <br/>
+          марки LIQUI MOLY
+          </p>
+        </div>
+      </div>
+      <div class="frame-6-fhZ">
+        <div class="group-27-d8b">
+          <img class="group-28-Bfu" src="./assets/group-28-yCK.png"/>
+          <p class="motul-v7h">
+          Официальный дистрибьютор материалов 
+          <br/>
+          марки Motul
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="auto-group-59hy-BpK">
+      <div class="group-27-v1D">
+        <img class="group-28-GKy" src="./assets/group-28-rvT.png"/>
+        <img class="rectangle-53-bNF" src="./assets/rectangle-53.png"/>
+        <p class="item--8sy">
+        Официальный дистрибьютор материалов 
+        <br/>
+        марки ШЕЛЛ
+        </p>
+        <img class="c3c8bdcb80a5922674bb45e84a130a0-1-kPZ" src="./assets/c3c8bdcb80a5922674bb45e84a130a0-1.png"/>
+      </div>
+      <div class="frame-3-G71">
+        <div class="group-27-Dnw">
+          <img class="group-28-z31" src="./assets/group-28-NqV.png"/>
+          <p class="castrol-Kqy">
+          Официальный 
+          <br/>
+          партнер Castrol
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="frame-1-DAf">
+      <div class="group-27-MGs">
+        <img class="group-28-SZD" src="./assets/group-28-88X.png"/>
+        <p class="elf-aQX">
+        Официальный дистрибьютор материалов 
+        <br/>
+        марки ELF
+        </p>
+      </div>
+    </div>
+    <div class="item--GYF">
+      <div class="item--NrB">ЛИЦЕНЗИИ</div>
+      <div class="rectangle-25-VA7">
+      </div>
+    </div>
+  </div>
+
+  </div>
+
+)}
+
+export default MainThirdScreen;   
