c9401e0c13e9452ae039056c1d33da21454fbc95
Strnikolay <nikolayromanchuk88@gmail.com>
2023-11-03 23:53:37 +0300

    03_11_23

---
+++ src/pages/about/currentNewsScreen/currentNewsScreen.jsx
@@ -0,0 +1,40 @@
+import "./screen3.css"
+
+function MainThirdScreen () {
+
+return(
+  
+  <div className="screen3">
+    
+
+  <div class="item--aoH">
+    <div class="item--5zw">
+      <div class="item--oR9">НОВОСТИ</div>
+      <div class="rectangle-25-K8b">
+      </div>
+      <p class="pro-auto-2Yo">Следите за новостями Pro auto</p>
+    </div>
+    <div class="item--LpP">
+      <div class="image-t5D">
+        <img class="logo-ohy" src="./logo-pvs.png"/>
+      </div>
+      <p class="item-24-2020--ia3">24 февраля 2020 года</p>
+      <p class="item--psy">Мы привыкли эксплуатировать автомобиль, пока что-либо не сломается. Так и со щётками - не заменим, пока совсем не потеряем возможность видеть перед собой из-за грязного лобового стекла. Следите за своими щётками и меняйте их каждый сезон, ведь безопасность на дороге превыше всего!</p>
+    </div>
+    <div class="item--rLo">
+      <div class="left-AsH">
+        <img class="vector-22-VPm" src="./vector-22.png"/>
+      </div>
+      <p class="item--ofM">ПРЕДЫДУЩИЕ</p>
+      <p class="item--CxP">СЛЕДУЮЩИЕ</p>
+      <div class="right-v7h">
+        <img class="vector-22-3i7" src="./vector-22-UZD.png"/>
+      </div>
+    </div>
+  </div>
+
+  </div>
+
+)}
+
+export default MainThirdScreen;   
