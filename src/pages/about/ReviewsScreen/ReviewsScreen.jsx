1bd4df3d8142061346e93c409879790675938d4e
Strnikolay <nikolayromanchuk88@gmail.com>
2023-11-03 23:52:44 +0300

    03_11_23

---
+++ src/pages/about/ReviewsScreen/ReviewsScreen.jsx
@@ -0,0 +1,43 @@
+import "./ReviewsScreen.css"
+
+function ReviewsScreen () {
+
+return(
+  
+  <div className="screen3">
+    
+
+
+  <div class="item--7vo">
+    <img class="item--Sy5" src="./assets/-aU7.png"/>
+    <div class="auto-group-d4x7-Nrj">
+      <div class="item--ubm">
+        <img class="vector-22-PG3" src="./assets/vector-22-SxP.png"/>
+      </div>
+      <div class="item--vFy">
+        <img class="vector-22-247" src="./assets/vector-22-amD.png"/>
+      </div>
+      <div class="item--MMH">
+        <img class="item--FxT" src="./assets/-YPH.png"/>
+        <p class="item--mR1">Обратился за новыми каркасными щётками, в итоге уехал и со щётками и с крутыми меховушками! Самый первый бокс на рынке на Нагорном, и с адекватными ценами! Обращаться к Андрею, очень толковый и отзывчивый продавец!</p>
+        <div class="rectangle-53-3fM">
+        </div>
+        <p class="item--Mvw">Сергей </p>
+        <p class="item--tAB">Частный клиент</p>
+      </div>
+    </div>
+    <div class="button-black-zU7">ОСТАВИТЬ СВОЙ ОТЗЫВ</div>
+    <div class="item--GRd">
+      <div class="item--aBR">ОТЗЫВЫ</div>
+      <div class="rectangle-25-Unb">
+      </div>
+      <p class="item--P8s">Для нас ценно любое Ваше мнение.</p>
+    </div>
+  </div>
+
+
+  </div>
+
+)}
+
+export default ReviewsScreen;   
---
+++ src/pages/main/PartnersScreen/PartnersScreen.jsx
@@ -0,0 +1,64 @@
+import "./PartnersScreen.css"
+
+function PartnersScreen () {
+
+return(
+  
+  <div className="screen3">
+
+
+
+    
+
+  <div class="item--dXD">
+    <div class="button-MCK">СТАТЬ ПАРТНЕРОМ</div>
+    <div class="rectangle-25-DVR">
+    </div>
+    <div class="item--xC7">ПАРТНЕРЫ</div>
+    <div class="item--E9d">
+      <img class="item-1-umZ" src="./assets/-6p7.png"/>
+      <div class="auto-group-txhh-EJ3">Компания «ПартКом»</div>
+    </div>
+    <div class="auto-group-7wjz-6LF">
+      <div class="item--cZV">
+        <img class="item-1-Kio" src="./assets/-fd1.png"/>
+        <div class="auto-group-11wd-em5">
+          <div class="item--NSB">ООО «Аккумуляторный Мир»</div>
+        </div>
+      </div>
+      <div class="item--SS3">
+        <img class="item-1-xQP" src="./assets/-i8f.png"/>
+        <div class="auto-group-y8f9-grB">
+          <div class="rectangle-57-dmR">
+          </div>
+          <div class="item--Nyu">Компания «ЮНИКС» г. Киров</div>
+        </div>
+      </div>
+      <div class="item--ctF">
+        <img class="item-1-jT5" src="./assets/-tLo.png"/>
+        <div class="auto-group-xsfq-ea3">Компания «Фаворит»</div>
+      </div>
+    </div>
+    <div class="auto-group-fshh-jLb">
+      <div class="item--47y">
+        <img class="item-1-aMD" src="./assets/-VwD.png"/>
+        <div class="auto-group-axpx-hwd">Компания «АРМТЕК»</div>
+      </div>
+      <div class="item--b1R">
+        <img class="item-1-WPH" src="./assets/-agf.png"/>
+        <div class="auto-group-j46s-chD">Компания «Бином»</div>
+      </div>
+      <div class="item--FEP">
+        <img class="item-1-jvF" src="./assets/-xDu.png"/>
+        <div class="auto-group-oa5d-smZ">Компания «Маслон»</div>
+      </div>
+    </div>
+  </div>
+
+
+    
+  </div>
+
+)}
+
+export default PartnersScreen;   
