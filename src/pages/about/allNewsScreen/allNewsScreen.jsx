8178d89d40b7d7dec9966d3bbdcf0801ddd7eed1
Strnikolay <nikolayromanchuk88@gmail.com>
2023-11-03 23:54:21 +0300

    03_11_23

---
+++ src/pages/about/allNewsScreen/allNewsScreen.jsx
@@ -0,0 +1,67 @@
+import "./allNewsScreen.css"
+
+function MainThirdScreen () {
+
+return(
+  
+  <div className="screen3">
+    
+
+    
+  <div class="item--WW3">
+    <div class="item--nCf">
+      <div class="item--HQK">НОВОСТИ</div>
+      <div class="rectangle-25-AU7">
+      </div>
+      <p class="pro-auto-t9D">Следите за новостями Pro auto</p>
+    </div>
+    <div class="item--bJX">
+      <div class="auto-group-osdr-h6f">
+        <img class="logo-Qmm" src="./logo-aKZ.png"/>
+      </div>
+      <div class="auto-group-scru-68o">
+        <p class="item-24-2020--c79">24 февраля 2020 года</p>
+        <p class="item--JEs">МЫ ОБНОВИЛИСЬ. ТЕПЕРЬ У НАС НОВЫЙ ЛОГОТИП И ОБНОВЛЕННЫЙ САЙТ.</p>
+        <p class="item--K9y">Мы рады ононсировать наш обновленный внешний мир. Мы не стоим на месте, постоянно развиваемся и улучшаем качество наших услуг, а также следим и за своим внешним видом.</p>
+        <div class="button-XfM">ЧИТАТЬ ПОДРОБНЕЕ</div>
+      </div>
+    </div>
+    <div class="auto-group-4xwh-nLP">
+      <div class="item-1-77m">
+        <div class="auto-group-gipp-Ru9">
+          <div class="item--aGF">
+            <img class="logo-gaB" src="./logo.png"/>
+          </div>
+        </div>
+        <div class="item--z55">
+          <p class="item-24-2020--5sD">24 февраля 2020 года</p>
+          <p class="item--NbR">У НАС НОВЫЙ ЛОГОТИП И НОВЫЙ САЙТ!</p>
+          <img class="union-SLP" src="./union-Bbu.png"/>
+        </div>
+      </div>
+      <div class="item-2-ZA7">
+        <img class="varta-facebook-bit-1-1400x920-1-6A3" src="./varta-facebook-bit-1-1400x920-1.png"/>
+        <div class="item--DVZ">
+          <p class="item-10-2020--v95">10 февраля 2020 года</p>
+          <p class="vartra-oCs">ОБНОВЛЕНИЕ ЛИНЕЙКИ АККУМУЛЯТОРОВ VARTRA </p>
+          <img class="union-tk7" src="./union-PbM.png"/>
+        </div>
+      </div>
+      <div class="item-3-E3H">
+        <img class="shutterstock722489089-1-ASj" src="./shutterstock722489089-1.png"/>
+        <div class="item--Joq">
+          <p class="item-5-2020--Dfu">5 февраля 2020 года</p>
+          <p class="online-KU3">БЕСПЛАТНАЯ КОНСУЛЬТАЦИЯ СПЕЦИАЛИСТА ТЕПЕРЬ В РЕЖИМЕ ONLINE</p>
+          <img class="union-CXq" src="./union.png"/>
+        </div>
+      </div>
+    </div>
+    <div class="button-KsM">ЕЩЕ БОЛЬШЕ НОВОСТЕЙ </div>
+  </div>
+
+
+  </div>
+
+)}
+
+export default MainThirdScreen;   
