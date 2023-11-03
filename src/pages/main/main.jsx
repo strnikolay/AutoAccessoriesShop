f130a801e33a196ba285176cec8ebe41dc78fec2
Strnikolay <nikolayromanchuk88@gmail.com>
2023-11-03 23:52:04 +0300

    03_11_23

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -1,5 +1,5 @@
 
-/*import {Header} from "../../components/header/header"
+import {Header} from "../../components/header/header"
 import MainFirstScreen from "./scr1/screen1";
 import MainSecondScreen from "./scr2/screen2";
 import MainThirdScreen from "./scr3/screen3";
@@ -7,9 +7,9 @@ import MainFourScreen from "./scr4/screen4";
 
 import MainFooter from "../../components/footer/footer";
 import ScrollButton from "../../components/scrollto/scrollbutton";
-import MainSixScreen from "./scr6/screen6";
-import MainAboutScreen from "./scr6 copy/main-about-screen";*/
 
+import MainAboutScreen from "./scr7/main-about-screen";
+import MainSixScreen from "./scr6/screen6";
 import VertGoodCard from "../../components/vert-good-card/vert-good-card";
 
 
@@ -17,14 +17,20 @@ function Main () {
 
 return(    
 <>
-<VertGoodCard />
-{/*<Header/>
+
+
+
+
+{/*
+<MainSixScreen/>
 <MainAboutScreen />
 <MainFirstScreen/>
 <MainSecondScreen/>
 <MainThirdScreen/>
 <MainFourScreen />
-<MainSixScreen/>
+<Header/>
+<VertGoodCard />
+
 <MainFooter/>
 <ScrollButton/>*/}
 </>
