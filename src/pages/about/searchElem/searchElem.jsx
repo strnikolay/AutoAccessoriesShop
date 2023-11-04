0fb5a71b76cdd46c39b3ce0731f6f378129a6d52
Strnikolay <nikolayromanchuk88@gmail.com>
2023-11-04 14:04:01 +0300

    доделываю верстку

---
+++ src/pages/about/searchElem/searchElem.jsx
@@ -0,0 +1,47 @@
+import "./screen3.css"
+
+function MainThirdScreen () {
+
+return(
+  
+  <div className="screen3">
+    
+
+  <div class="item--4oR">
+    <div class="item--Ldm">
+      <div class="auto-group-28yd-rs1">
+        <div class="auto-group-mfyz-myy">
+          <img class="item--n8P" src="./assets/-tV5.png"/>
+        </div>
+        <div class="button-VoV">НАЙТИ</div>
+        <img class="item--nXh" src="./assets/-Fd9.png"/>
+      </div>
+      <div class="rectangle-5-JW3">
+      </div>
+    </div>
+  </div>
+  <div class="frame-47-dHR">
+    <div class="auto-group-dauw-Mz7">
+      <div class="auto-group-s1yk-gWb">
+        <span class="auto-group-s1yk-gWb-sub-0">Гелиевые </span>
+        <span class="auto-group-s1yk-gWb-sub-1">Аккумуляторы </span>
+        <span class="auto-group-s1yk-gWb-sub-2">OPTIMA</span>
+      </div>
+      <div class="auto-group-jmmh-jtB">
+        <span class="auto-group-jmmh-jtB-sub-0">Оригинальные </span>
+        <span class="auto-group-jmmh-jtB-sub-1">Аккумуляторы</span>
+      </div>
+      <div class="auto-group-1hg3-7dq">Аккумуляторы</div>
+      <div class="auto-group-rt9r-R8j">Все результаты</div>
+    </div>
+    <div class="rectangle-5-vLP">
+    </div>
+  </div>
+
+
+
+  </div>
+
+)}
+
+export default MainThirdScreen;   
