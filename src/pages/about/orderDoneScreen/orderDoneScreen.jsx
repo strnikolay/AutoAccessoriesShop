df7a6edd05406b303f41cb8afdcf922ef35bfa0a
Strnikolay <nikolayromanchuk88@gmail.com>
2023-11-04 14:05:10 +0300

    доделываю верстку

---
+++ src/pages/about/orderDoneScreen/orderDoneScreen.jsx
@@ -0,0 +1,31 @@
+import "./screen3.css"
+
+function MainThirdScreen () {
+
+return(
+  
+  <div className="screen3">
+    
+  <div class="item--gBR">
+    <div class="item--YzK">ОФОРМЛЕНИЕ ЗАКАЗА</div>
+    <div class="item--Daf">
+      <div class="mask-group-KNo">
+        <img class="auto-group-ni23-GJ3" src="./assets/auto-group-ni23.png"/>
+      </div>
+      <div class="auto-group-imnf-Nrs">
+        <p class="item-520--ivj">Ваш заказ №520 успешно оформлен</p>
+        <p class="item--bDq">Благодарим за выбор нашей компании. В ближайшее время наши менеджеры свяжуться с вами, ответят на любые вопросы и подготовят счет на оплату.</p>
+        <div class="auto-group-b7bd-sKm">
+          <div class="button-2-hover-Qab">В КАТАЛОГ</div>
+          <div class="button-black-HeP">НА ГЛАВНУЮ</div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  </div>
+
+)}
+
+export default MainThirdScreen;   
