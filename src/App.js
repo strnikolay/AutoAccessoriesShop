c56bcecefaa08030cb5ceb82f4cb9d5b9fb18c5f
Strnikolay <nikolayromanchuk88@gmail.com>
2023-11-28 17:16:07 +0300

    28_11_23

--- src/App.js
+++ src/App.js
@@ -2,29 +2,25 @@ import {createBrowserRouter, RouterProvider } from "react-router-dom";
 import './App.css';
 import "./images/Geometria-Bold.ttf"
 
-import About from "./pages/about/about";
-//import Main  from './pages/main/main';
-import Catalog from "./pages/catalog/catalog";
+//import About from "./pages/about/about";
+import Main  from './pages/main/main';
+//import Catalog from "./pages/catalog/catalog";
 import ErrorPage from "./pages/404/404";
-
-
-
-
-
 
 function App() {
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element:<Catalog/>,
-      //element: <Main/>,
+      //element:<Catalog/>,
+      element: <Main/>,
+      //element: <ErrorPage />,
       errorElement: <ErrorPage />,
     },
-    {
+    /*{
       path: "/about",
       element: <About />,
-    },
+    },*/
   ]);
 
   return (
--- src/pages/main/scr1/screen1.jsx
+++ src/pages/main/scr1/screen1.jsx
@@ -1,21 +1,55 @@
+import React from "react";
 import "./screen1.css"
 import Button from "../../../components/button/button";
 import Blok2  from "./blok";
 
 function MainFirstScreen () {
+  const [i, setI] = React.useState(0)
+
+
+
+  const text = [
+    "надежные, качественные аккумуляторы",
+    "профессиональная помощь вашему автомобилю",
+    "команда высококласных специалистов",
+    "заботливый сервис и своевренная информация"
+  ]
+
+  const el = document.querySelectorAll(".rect");
+  
+  React.useEffect(() => {
+    const timer = setTimeout(()=>{
+      console.log(el)
+      i<3 ? setI(i+1) : setI(0);
+      if (el.length > 0) {el.forEach((el) => el.style = "background-color: #fffff;")
+      el[i].style = "background-color: #C53720;"}
+    }, 2000)
+    return () => clearTimeout(timer)
+
+  }, [i])
+
+  
 
 return(
   
 <div className="screen-1">
-  <div className="screen-1-wraper">
-    <div className="screen-1-background">
+  <div className={"screen-1-bg background"+i}>
+  <div className="content-wraper">
+
       <div className="title">
-        <h1></h1>
+        <h1>{text[i].toUpperCase()}</h1>
       </div>
-      <Button />
+      <Button text="ПЕРЕЙТИ В КАТАЛОГ"/>
+      <div className="rect-wrapper">
+        <div className="rect"/>
+        <div className="rect"/>
+        <div className="rect"/>
+        <div className="rect"/>
+      </div>
     </div>
-    <Blok2/>
+
 </div>
+  <Blok2/>
 </div>
 )}
 
