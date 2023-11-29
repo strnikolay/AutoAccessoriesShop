320dceda715074473acb903b5161d15d14455ae9
Strnikolay <nikolayromanchuk88@gmail.com>
2023-11-29 14:29:22 +0300

    29_11_23

--- src/pages/main/scr1/blok.jsx
+++ src/pages/main/scr1/blok.jsx
@@ -8,7 +8,7 @@ import online from "./img/online.svg"
 const Blok2 = () => {
 return (
 <div className="screen">
-    <div className="wrapper">
+  <div className="wrapper">
     <div className="view">
         <img className="ico" src={cash}/>
         <div className="text-wrapper">Наличный и безналичный расчет</div>
@@ -25,8 +25,8 @@ return (
         <img className="ico" src={online} />
         <p className="text-wrapper">Мы всегда на связи с 9:00 до 18:00!</p>
     </div>
-    </div>
+  </div>
 </div>
-);
+)
 };
 export default Blok2;
--- src/pages/main/scr1/screen1.jsx
+++ src/pages/main/scr1/screen1.jsx
@@ -6,8 +6,6 @@ import Blok2  from "./blok";
 function MainFirstScreen () {
   const [i, setI] = React.useState(0)
 
-
-
   const text = [
     "надежные, качественные аккумуляторы",
     "профессиональная помощь вашему автомобилю",
@@ -16,17 +14,17 @@ function MainFirstScreen () {
   ]
 
   const el = document.querySelectorAll(".rect");
-  
+
   React.useEffect(() => {
     const timer = setTimeout(()=>{
-      console.log(el)
       i<3 ? setI(i+1) : setI(0);
+      //title.style = "animation: blurFadeInOut 3s ease-in backwards;"
       if (el.length > 0) {el.forEach((el) => el.style = "background-color: #fffff;")
       el[i].style = "background-color: #C53720;"}
-    }, 2000)
+    }, 5000)
     return () => clearTimeout(timer)
 
-  }, [i])
+  }, [i, el])
 
   
 
@@ -39,7 +37,9 @@ return(
       <div className="title">
         <h1>{text[i].toUpperCase()}</h1>
       </div>
-      <Button text="ПЕРЕЙТИ В КАТАЛОГ"/>
+      <div className="button-wrapper">
+        <Button text="ПЕРЕЙТИ В КАТАЛОГ"/>
+      </div>
       <div className="rect-wrapper">
         <div className="rect"/>
         <div className="rect"/>
