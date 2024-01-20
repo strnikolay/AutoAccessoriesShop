27738fae5c1ec1c1c7645c44a69877eb5f77cf9f
Проходная <nikolayromanchuk88@gmail.com>
2024-01-20 22:42:47 +0300

    remove some

---
+++ src/pages/new/phoneCallBackScreen/phoneCallBackScreen.jsx
@@ -0,0 +1,88 @@
+import "./screen3.css"
+
+function MainThirdScreen () {
+
+return(
+  
+  <div className="screen3">
+    
+
+  <div class="item--Tcs">
+    <div class="item--NE3">
+      <div class="item--rf1">
+        <div class="rectangle-25-wwM">
+        </div>
+        <div class="item--51y">Задать вопрос</div>
+        <div class="item--VbV">
+          <p class="item--zYF">Заказать услугу</p>
+          <div class="rectangle-91-t7q">
+          </div>
+        </div>
+      </div>
+      <div class="item--cJj">Данные для заказа</div>
+      <div class="auto-group-avk9-5CK">
+        <div class="item--ncX">
+          <div class="item--5bd">
+            <div class="auto-group-udb5-CgF">
+              <div class="item--XTd">
+                <span class="item--XTd-sub-0">Ваше имя:</span>
+                <span class="item--XTd-sub-1">*</span>
+              </div>
+              <div class="item--K27">Заполниет поле</div>
+            </div>
+            <div class="item--1Qj">
+            </div>
+          </div>
+          <div class="item--Xdy">
+            <div class="auto-group-p95r-FK5">
+              <div class="item--acF">
+                <span class="item--acF-sub-0">Телефон:</span>
+                <span class="item--acF-sub-1">*</span>
+              </div>
+              <div class="item--6U7">Заполниет поле</div>
+            </div>
+            <div class="item-7-BkT">
+            </div>
+          </div>
+          <div class="email-ghD">
+            <div class="auto-group-drc7-2FH">
+              <div class="e-mail--MHZ">E-mail:</div>
+              <div class="item--gaj">Неверный формат</div>
+            </div>
+            <div class="item-8-NTZ">
+            </div>
+          </div>
+          <div class="item--Tju">
+            <div class="item--P7m">Услуга</div>
+            <div class="item-9-6ns">Диагностика аккумулятора автомобиля</div>
+          </div>
+          <div class="item--8jZ">
+            <div class="item--rfZ">Сообщение</div>
+            <div class="item-10-a5m">
+            </div>
+          </div>
+          <div class="item--US3">
+            <div class="ticker--mg3">
+              <div class="rectangle-61-Vc3">
+              </div>
+            </div>
+            <div class="item--26B">
+              <span class="item--26B-sub-0">Я согласен на </span>
+              <span class="item--26B-sub-1">обработку персональных данных</span>
+            </div>
+          </div>
+        </div>
+        <img class="image-JC7" src="./assets/image.png"/>
+      </div>
+      <div class="auto-group-jy7z-2du">
+        <div class="item--AEK">* Обязательное поле</div>
+        <div class="button-2-hover-qrF">ЗАКАЗАТЬ ЗВОНОК</div>
+      </div>
+    </div>
+  </div>
+
+  </div>
+
+)}
+
+export default MainThirdScreen;   
---
+++ src/pages/new/serviceMiniScreen/serviceMiniScreen.jsx
@@ -0,0 +1,56 @@
+import "./screen3.css"
+
+function MainThirdScreen () {
+
+return(
+  
+  <div className="screen3">
+    
+  <div class="item--YPH">
+    <div class="rectangle-25-QwH">
+    </div>
+    <p class="item-600--YGo">Наша компания специализируется скупке поддержанных АКБ. Прием бу аккумуляторов для авто – от 600 рублей.</p>
+    <div class="image-3cs">
+      <div class="rectangle-92-mom">
+      </div>
+      <p class="item-600--H1R">
+      ПОКУПАЕМ Б/У АККУМУЛЯТОРЫ 
+      <br/>
+      от 600 руб
+      </p>
+    </div>
+    <div class="item--5xs">
+      <p class="item--yYT">прием аккумуляторов</p>
+      <div class="rectangle-26-FF5">
+      </div>
+    </div>
+  </div>
+  <div class="item--Yjy">
+    <div class="item--9Us">
+      <p class="item--1X5">ДИАГНОСТИКА АККУМУЛЯТОРА</p>
+      <div class="rectangle-26-VBM">
+      </div>
+    </div>
+    <div class="banner-1fV">
+      <div class="rectangle-92-ZS7">
+      </div>
+      <p class="item--fzw">
+      ТРЕБУЕТ ЛИ ЗАМЕНЫ 
+      <br/>
+      АККУМУЛЯТОР?
+      </p>
+    </div>
+    <p class="pro-auto--uPV">
+    Профессионалы магазина «PRO auto» совершенно бесплатно проведут диагностику АКБ Вашего автомобиля, а также дадут рекомендации по дальнейшей эксплуатации аккумулятора, или по его замене, в случае необходимости.
+    <br/>
+    
+    <br/>
+    Ждём Вас в наших отделах продаж!
+    </p>
+  </div>
+
+  </div>
+
+)}
+
+export default MainThirdScreen;   
