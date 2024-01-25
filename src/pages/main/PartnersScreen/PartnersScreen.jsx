0d3c149dabbe5fa6181e9663088055359268366c
Strnikolay <nikolayromanchuk88@gmail.com>
2024-01-25 23:39:00 +0300

    25_01_24

--- src/pages/main/PartnersScreen/PartnersScreen.jsx
+++ src/pages/main/PartnersScreen/PartnersScreen.jsx
@@ -6,39 +6,38 @@ return(
   
   <div className="screen3">
 
-
-
-    
-
-  <div class="item--dXD">
-    <div class="button-MCK">СТАТЬ ПАРТНЕРОМ</div>
     <div class="rectangle-25-DVR">
-    </div>
-    <div class="item--xC7">ПАРТНЕРЫ</div>
-    <div class="item--E9d">
-      <img class="item-1-umZ" src="./assets/-6p7.png"/>
-      <div class="auto-group-txhh-EJ3">Компания «ПартКом»</div>
-    </div>
-    <div class="auto-group-7wjz-6LF">
-      <div class="item--cZV">
-        <img class="item-1-Kio" src="./assets/-fd1.png"/>
-        <div class="auto-group-11wd-em5">
-          <div class="item--NSB">ООО «Аккумуляторный Мир»</div>
-        </div>
+
+      <div class="item--xC7">ПАРТНЕРЫ</div>
+
+      <div class="item--E9d">
+        <img class="item-1-umZ" src="./assets/-6p7.png"/>
+        <div class="auto-group-txhh-EJ3">Компания «ПартКом»</div>
       </div>
-      <div class="item--SS3">
-        <img class="item-1-xQP" src="./assets/-i8f.png"/>
-        <div class="auto-group-y8f9-grB">
-          <div class="rectangle-57-dmR">
+
+      <div class="auto-group-7wjz-6LF">
+        <div class="item--cZV">
+          <img class="item-1-Kio" src="./assets/-fd1.png"/>
+          <div class="auto-group-11wd-em5">
+            <div class="item--NSB">ООО «Аккумуляторный Мир»</div>
+          </div>    
+        </div>
+        <div class="item--SS3">
+          <img class="item-1-xQP" src="./assets/-i8f.png"/>
+          <div class="auto-group-y8f9-grB">
+            <div class="rectangle-57-dmR">
           </div>
           <div class="item--Nyu">Компания «ЮНИКС» г. Киров</div>
         </div>
       </div>
+
       <div class="item--ctF">
         <img class="item-1-jT5" src="./assets/-tLo.png"/>
         <div class="auto-group-xsfq-ea3">Компания «Фаворит»</div>
       </div>
+
     </div>
+
     <div class="auto-group-fshh-jLb">
       <div class="item--47y">
         <img class="item-1-aMD" src="./assets/-VwD.png"/>
@@ -53,9 +52,11 @@ return(
         <div class="auto-group-oa5d-smZ">Компания «Маслон»</div>
       </div>
     </div>
+
   </div>
-
-
+  <div class="item--dXD">
+    <div class="button-MCK">СТАТЬ ПАРТНЕРОМ</div>
+  </div>
     
   </div>
 
--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -1,26 +1,28 @@
 
 import {Header} from "../../components/header/header"
 import MainFirstScreen from "./MainFirstScreen/MainFirstScreen";
+import PartnersScreen from "./PartnersScreen/PartnersScreen";
 
-import MainCatalogScreen from "./MainCatalogScreen/MainCatalogScreen";
+/*import MainCatalogScreen from "./MainCatalogScreen/MainCatalogScreen";
 import MainServiceScreen from "./MainServiceScreen/MainServiceScreen";
 import MainAboutScreen from "./MainAboutScreen/main-about-screen";
 import MainPartnerCarousel from "./PartnerCarousel/PartnerCarousel";
 import MainFooter from "../../components/footer/footer";
-import ScrollButton from "../../components/scrollto/scrollbutton";
+import ScrollButton from "../../components/scrollto/scrollbutton";*/
 
 function Main () {
 
 return(    
 <>
 <Header/>
-<MainFirstScreen/>
+<PartnersScreen/>
+{/*<MainFirstScreen/>
 <MainCatalogScreen/>
 <MainServiceScreen/>
 <MainAboutScreen />
 <MainPartnerCarousel/>
 <MainFooter/>
-<ScrollButton/>
+<ScrollButton/>*/}
 </>
 )}
  
