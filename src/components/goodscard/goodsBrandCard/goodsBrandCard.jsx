acdb8bc5ab3fe3f97d702ae73410b57754b3905f
Проходная <nikolayromanchuk88@gmail.com>
2024-01-28 23:58:44 +0300

    28_01_24

--- src/components/goodscard/goodsBrandCard/goodsBrandCard.jsx
+++ src/components/goodscard/goodsBrandCard/goodsBrandCard.jsx
@@ -5,14 +5,14 @@ function MainThirdScreen () {
 return(
   
   <div className="screen3">
-    <div class="item--oZy">
-  <img class="magnum-1-6JB" src="./assets/magnum-1-jrX.png"/>
-  <div class="auto-group-awjr-DNo">
-    <p class="magnum-LiK">АКБ MAGNUM</p>
-    <p class="item--fEo">Производство: Казахстан, Компания «Кайнар – АКБ»</p>
-    <div class="button-76o">ПОДРОБНЕЕ</div>
-  </div>
-</div>
+    <div class="brand-card-wrapper">
+      <img class="brand-card-img" src="./assets/magnum-1-jrX.png"/>
+      <div class="auto-group-awjr-DNo">
+        <p class="magnum-LiK">АКБ MAGNUM</p>
+        <p class="item--fEo">Производство: Казахстан, Компания «Кайнар – АКБ»</p>
+        <div class="button-76o">ПОДРОБНЕЕ</div>
+      </div>
+    </div>
   </div>
 
 )}
